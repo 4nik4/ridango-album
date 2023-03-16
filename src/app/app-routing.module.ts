027d1960c40d0cd6d5d7d51602e6b35109b5450b
Anika <anika.erceg@gmail.com>
2023-03-16 11:58:37 +0200

    further init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './album/album.component';
+import { LogComponent } from './log/log.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'albums', component: AlbumComponent},
+  {path: 'logs', component: LogComponent},
+  {path: 'users', component: UsersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
