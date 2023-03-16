717a38c79ce433ba63b4aeb5e4355b1d1344a335
Anika <anika.erceg@gmail.com>
2023-03-16 11:12:06 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
+import { LogComponent } from './log/log.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent,
+    LogComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
